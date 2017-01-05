73b091738867e99a7136a8a04990ca8843100d01
BuildTools <marco.firsteye@gmail.com>
2017-01-05 15:15:47 -0800

    Added a new world

--- plugins/monstersplus/monsters/monsterevents/CaveSpiderSpawnEvent.java
+++
@@ -1,9 +0,0 @@
-package me.coolade.monstersplus.monsters.monsterevents;
-
-import org.bukkit.entity.LivingEntity;
-
-public class CaveSpiderSpawnEvent {
-	public CaveSpiderSpawnEvent(LivingEntity lent) {
-
-	}
-}
